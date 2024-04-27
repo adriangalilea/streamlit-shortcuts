8932c8b03b7d99e7f8df4778d4b110817c3e8b3d
adriangalilea <adriangalilea@gmail.com>
2024-04-27 23:30:46 +0200

    chore: setup 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='streamlit-shortcuts',
-    version='0.1.1',
+    version='0.1.2',
     author='Adrian Galilea Delgado',
     author_email='adriangalilea@gmail.com',
     packages=find_packages(where="src"),
